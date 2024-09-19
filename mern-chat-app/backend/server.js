5bf944eabf47b11d860d07ac02c1b580bc594020
yohannes4321 <alemuyohannes960@gmail.com>
2024-09-19 12:43:43 +0300

    d

--- mern-chat-app/backend/server.js
+++ mern-chat-app/backend/server.js
@@ -21,18 +21,7 @@ app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
 
-// Deployment settings
-const __dirname1 = path.resolve();
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/frontend/build")));
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running..");
-  });
-}
+
 
 // Error Handling middlewares
 app.use(notFound);
